68acdb26d01d058184674058fdb622a0be4de557
Sandra <sandra.rupo@gmail.com>
2024-11-05 17:53:08 +0100

    Joc jugable v1

--- DodgeTheCreepSandra/Main.gd
+++ DodgeTheCreepSandra/Main.gd
@@ -5,16 +5,20 @@ var score
 
 func _ready():
 	randomize()
+	new_game()
 
 
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 func _on_ScoreTimer_timeout():
 	score += 1
@@ -22,6 +26,7 @@ func _on_ScoreTimer_timeout():
 func _on_StartTimer_timeout():
 	$MobTimer.start()
 	$ScoreTimer.start()
+	$HUD.update_score(score)
 	
 func _on_MobTimer_timeout():
 	# Crea una nova instància de l'escena Mob.
